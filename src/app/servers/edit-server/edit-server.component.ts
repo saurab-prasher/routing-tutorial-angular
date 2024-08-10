ac1216ac88410bbc891b76a2465e5ebe36910785
saurabprasher <saurab015@gmail.com>
2024-08-09 21:34:29 -0400

    routing-tutorial

---
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-server',
+  templateUrl: './edit-server.component.html',
+  styleUrls: ['./edit-server.component.css'],
+})
+export class EditServerComponent implements OnInit {
+  server: { id: number; name: string; status: string };
+  serverName = '';
+  serverStatus = '';
+  allowEdit = false;
+
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.server = this.serversService.getServer(1);
+    this.serverName = this.server.name;
+    this.serverStatus = this.server.status;
+    this.route.queryParams.subscribe((params: Params) => {
+      this.allowEdit = params['allowEdit'] === '1';
+    });
+  }
+
+  onUpdateServer() {
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus,
+    });
+  }
+}
